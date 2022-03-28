cc99950f8ce7bb9e31bb76edfa644800736d6da8
Simon <simon@ml-gis-service.com>
2022-03-28 13:15:24 +0300

    Update paths to static files and first demo

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-import os
-
 import hydra
-from hydra.utils import get_original_cwd, to_absolute_path
+from hydra.utils import to_absolute_path
 from omegaconf import DictConfig
 from predictions import forecast
 from predictions.utils import get_frame, get_series
 from predictions.viz.show import plot_single
 
 
+def transform_relative_path(relative_path: str):
+    """
+    Function gets absolute path to the static files used in a predictions.
+
+    :param relative_path: (str) path to the static files from the main script directory.
+    :return: (str)
+    """
+    abs_path = to_absolute_path(relative_path)
+    return abs_path
+
+
 @hydra.main(config_path='conf', config_name='config')
 def run_prediction(cfg: DictConfig) -> None:
-    print('Experiment data directory:', cfg.experiment.data_loc)
-    print('Current working directory:', os.getcwd())
-    print('Original working directory:', get_original_cwd())
-    print('Absolute path to the data file:', to_absolute_path(cfg.experiment.data_loc))
-    sample = get_frame(cfg.experiment.data_loc)
+    data_path = transform_relative_path(cfg.experiment.data_loc)
+    sample = get_frame(data_path)
     sample_series = get_series(sample, cfg.experiment.date_col, cfg.experiment.value_col)
     pred = forecast(sample_series,
                     cfg.experiment.steps_ahead,

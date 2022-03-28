8ac147440507dbe305cd1834e774d68fb424ff48
Simon <simon@ml-gis-service.com>
2022-03-28 12:45:53 +0300

    config-main-first-step

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import hydra
+from omegaconf import DictConfig
 from predictions import forecast
 from predictions.utils import get_frame, get_series
+from predictions.viz.show import plot_single
 
 
-def run_prediction() -> None:
-    steps_ahead = 15
-    time_freq = 'D'
-    sample = get_frame('data/dev/input_temperature.csv')
-    sample_series = get_series(sample, 'time', 'temperature')
-    naive_pred = forecast(sample_series, steps_ahead, time_freq, 'naive')
+@hydra.main(config_path='conf', config_name='config')
+def run_prediction(cfg: DictConfig) -> None:
+    sample = get_frame(cfg.experiment.data_loc)
+    sample_series = get_series(sample, cfg.experiment.date_col, cfg.experiment.value_col)
+    pred = forecast(sample_series,
+                    cfg.experiment.steps_ahead,
+                    cfg.experiment.frequency,
+                    cfg.experiment.model)
+    if cfg.experiment.plot:
+        plot_single(pred)
 
 
 if __name__ == '__main__':

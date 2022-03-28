fdde405286b1e0d176eae6f9cef981b39a45dac6
Simon <simon@ml-gis-service.com>
2022-03-28 13:21:59 +0300

    run - refactor base

--- main.py
+++ main.py
@@ -19,14 +19,28 @@ def transform_relative_path(relative_path: str):
 
 @hydra.main(config_path='conf', config_name='config')
 def run_prediction(cfg: DictConfig) -> None:
-    data_path = transform_relative_path(cfg.experiment.data_loc)
+    # Vars: data
+    data_path = transform_relative_path(cfg.data.loc)
+    date_col_name = cfg.data.date_col
+    value_col_name = cfg.data.value_col
+    # Vars: model
+    steps = cfg.experiment.steps_ahead
+    freq = cfg.experiment.frequency
+    model_type = cfg.experiment.model
+    # Vars: app
+    is_plot = cfg.app.plot
+
+    # Run: get data
     sample = get_frame(data_path)
-    sample_series = get_series(sample, cfg.experiment.date_col, cfg.experiment.value_col)
+    sample_series = get_series(sample, date_col_name, value_col_name)
+    # Run: model
     pred = forecast(sample_series,
-                    cfg.experiment.steps_ahead,
-                    cfg.experiment.frequency,
-                    cfg.experiment.model)
-    if cfg.experiment.plot:
+                    steps_ahead=steps,
+                    freq=freq,
+                    model_type=model_type)
+
+    # Run: app
+    if is_plot:
         plot_single(pred)
 
 

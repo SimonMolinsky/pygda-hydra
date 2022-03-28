b78e58638e722c75bf31c601ee634388200abbae
Simon <simon@ml-gis-service.com>
2022-03-28 13:08:30 +0300

    path investigation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
+
 import hydra
+from hydra.utils import get_original_cwd, to_absolute_path
 from omegaconf import DictConfig
 from predictions import forecast
 from predictions.utils import get_frame, get_series
@@ -7,6 +10,10 @@ from predictions.viz.show import plot_single
 
 @hydra.main(config_path='conf', config_name='config')
 def run_prediction(cfg: DictConfig) -> None:
+    print('Experiment data directory:', cfg.experiment.data_loc)
+    print('Current working directory:', os.getcwd())
+    print('Original working directory:', get_original_cwd())
+    print('Absolute path to the data file:', to_absolute_path(cfg.experiment.data_loc))
     sample = get_frame(cfg.experiment.data_loc)
     sample_series = get_series(sample, cfg.experiment.date_col, cfg.experiment.value_col)
     pred = forecast(sample_series,
